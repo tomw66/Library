17d309ba1ce8476564b7673188067ab5569e51c0
tomw66 <tom.wilson96@hotmail.com>
2021-07-11 11:53:44 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+      return title + " by " + author + ", " + pages + " pages, " + read
+    }
+  }
+
+  const theHobbit = new Book('The hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
